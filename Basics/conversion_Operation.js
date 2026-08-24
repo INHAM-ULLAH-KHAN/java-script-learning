3157787260fc5f9a806b80f68d403d3e8b5873a4
INHAM-ULLAH-KHAN <157222089+INHAM-ULLAH-KHAN@users.noreply.github.com>
2026-08-24 18:23:10 +0000

    Learn Opeartion

--- Basics/conversion_Operation.js
+++ Basics/conversion_Operation.js
@@ -1,10 +1,10 @@
 let score = "Inham"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" can be converted into 33
@@ -13,8 +13,53 @@ console.log(valueInNumber);
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // " " is false 
 // "an" true 
 // 1 is true 
 //  0 is false 
+
+
+
+/* Operations*/
+let val = 3
+let negVal = -val
+
+// console.log(negVal);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "Hi"
+let str2 = " Inham"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1" + 2 + 4);
+
+// console.log(+true);//true
+// console.log(+"");//false
+
+let num1, num2, num3 = 2 + 2 
+let gameCounter = 1
+// // gameCounter++ /postfix
+// ++gameCounter // prefix
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(y);
+console.log(x);
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
