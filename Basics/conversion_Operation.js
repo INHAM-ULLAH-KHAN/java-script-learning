37eea60461639cda2d5419af465373431bad3c85
INHAM-ULLAH-KHAN <157222089+INHAM-ULLAH-KHAN@users.noreply.github.com>
2026-08-24 18:00:57 +0000

    Learn conversion

---
+++ Basics/conversion_Operation.js
@@ -0,0 +1,20 @@
+let score = "Inham"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" can be converted into 33
+// 33abc is NaN 
+// True/false 1/0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// " " is false 
+// "an" true 
+// 1 is true 
+//  0 is false 
